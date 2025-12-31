e4cfa2e71cc98705b34812085897c10384b7631b
arzzy81 <123502422+arzzy81@users.noreply.github.com>
2025-12-31 19:37:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -206,10 +206,14 @@ export default function App() {
            </header>
         
         <div className="text-center mb-12 md:mb-20 relative">
-         <h2 className="text-[80px] md:text-[140px]  bg-gradient-to-r from-[#2FA4FF] to-[#8B5CF6] bg-clip-text text-transparent font-black" style={{ fontFamily: "'Crimson Text', serif" }}>
-          {userName ? `${userName.toUpperCase()}, VOCÊ JÁ LEU ${readingPercentage}%` : `VOCÊ JÁ LEU ${readingPercentage}%`}
-        </h2>
-         
+          <p className="text-[60px] tracking-[0.5em] text-[#2FA4FF] font-black uppercase mb-4">
+    Progresso de Leitura
+          </p>
+          <h2 className="text-[80px] md:text-[140px] bg-gradient-to-r from-[#2FA4FF] to-[#8B5CF6] bg-clip-text text-transparent font-black"
+          style={{ fontFamily: "'Crimson Text', serif" }}>
+    {userName? `${userName.toUpperCase()}, VOCÊ JÁ LEU ${readingPercentage}%`: `VOCÊ JÁ LEU ${readingPercentage}%`}
+          </h2>
+        </div>
 
         <div className="mb-12 md:mb-20">
           <DailyReadingCard

b165cdd2766508a523d9c4363bdb9846789c5839
arzzy81 <123502422+arzzy81@users.noreply.github.com>
2025-12-31 18:12:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -195,9 +195,11 @@ export default function App() {
         
         <header className="mb-12 md:mb-16 max-w-2xl">
           <h1 className="text-[28px] md:text-[52px] leading-[1.1] mb-6 font-light italic" style={{ fontFamily: "'Crimson Text', serif" }}>
+           <br>
             Um dia por vez. 
             Um texto por dia. 
             Uma vida transformada.
+           </br>
           </h1>
           <p className="text-[14px] md:text-[18px] text-[#DADADA] border-l-2 border-[#2FA4FF]/30 pl-6">
             Quando a Palavra ocupa um lugar diário na rotina, o entendimento é ampliado...

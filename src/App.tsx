4a5d5ce38151e9b8066684f01f1a443699aea749
arzzy81 <123502422+arzzy81@users.noreply.github.com>
2025-12-31 18:51:04 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -210,7 +210,7 @@ export default function App() {
           <p className="text-[10px] tracking-[0.5em] text-[#2FA4FF] font-black uppercase mb-4">
             Progresso de Leitura
           </p>
-       <h2 className="text-[60px] md:text-[100px] bg-gradient-to-r from-[#2FA4FF] to-[#8B5CF6] leading-[0.9] bg-clip-text text-transparent font-black" style={{ fontFamily: "'Crimson Text', serif" }}>
+       <h2 className="text-[80px] md:text-[140px] bg-gradient-to-r from-[#2FA4FF] to-[#8B5CF6] bg-clip-text text-transparent font-black" style={{ fontFamily: "'Crimson Text', serif" }}>
             {userName ? `${userName.toUpperCase()}, VOCÊ JÁ LEU ${readingPercentage}%` : `VOCÊ JÁ LEU ${readingPercentage}%`}
        </h2>
         </div>
